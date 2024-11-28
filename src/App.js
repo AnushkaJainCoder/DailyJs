b82fd286f763bbd7b465c2f55a4becbb5538b675
anushka.jain <anushka.jain@innovasolutions.com>
2024-11-28 17:43:44 +0530

    react topics

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+import { type } from '@testing-library/user-event/dist/type';
+import React, { Children } from 'react';
+import Quotes from './Components/quotes';
+import Button from './Components/quotes';
+
+function App() {
+
+  // const Button = ({lebel, click}) => {
+  //   return <button onClick={click}>{lebel}</button>
+  // }
+  const user = {
+    name: 'Anushka Jain',
+    ppo: 20000000
+  }
+  const el1 = (
+    <h1 className='josh'>I can do it and I will</h1>
+  )
+  const el = React.createElement(
+    'h1',
+    {className: 'josh'},
+    'I can do it and I will'
+  )
+  const el2 = {
+    type: 'h1',
+    props:{
+      className: 'josh',
+      Children: 'please give me some shine'
+    }
+    
+  }
+  function formatName(user){
+    if(user){
+        return (
+          <>
+           Hello {user.name} {user.ppo}
+          </>
+        )
+    }
+    else{
+      return (
+        <p>No user found</p>
+      )
+    }
+    
+  }
+
+  return (
+    <div className="App">
+      {/* <Button lebel={'click me'} click={() => alert('you clicked')} /> */}
+        <h1>{formatName() }</h1>
+
+
+{/* ---------------- Components with props-------------------*/}
+
+      {/* <Quotes name= {'Anushka'}/> */}
+      {/* <p>{count}</p> */}
+
+      <Button mssg = "Be happy Anushka "/>
+      
+
+    </div>
+  );
+}
+
+export default App;
