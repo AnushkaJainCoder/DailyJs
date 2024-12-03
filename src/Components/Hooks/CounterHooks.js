71ba7a1d569ad65750be5481c34f07186c19cb71
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-03 11:37:47 +0530

    use object for usestate initialization

--- src/Components/Hooks/CounterHooks.js
+++ src/Components/Hooks/CounterHooks.js
@@ -1,25 +1,45 @@
 import { useState } from "react"
 
 export default function CounterHooks(){
-    const initialFun = () =>{
-        console.log('initial fun');
-        
-        return 20
-    }
-    const [count, setCount] = useState(initialFun());
+
+    //--------------------------------- initialization using function
+        // const initialFun = () =>{
+        //     console.log('initial fun');
+            
+        //     return 20
+        // }
+        //-------------pass by reference useState(initialFun);
+        // const [count, setCount] = useState(initialFun());
+
+    //----------------------------------- initialization directly by passing value
+        // const [count, setCount] = useState(0);
+
+    // pass object
+    const [state, setState] = useState({count: 4, age: 20});
+    const count = state.count;
+    const age = state.age;
+
+
 
     const handleChange = () => {
-        // overriding the value of count each time, bec we are using the same value of count each time
-        // instead we can use the prev state value
-        // setCount(count+1);
-        // setCount(count+1);
-        // setCount(count+1);
 
-        setCount(prev => prev +1);
-        // setCount(prev => prev +1);
-        // setCount(prev => prev +1);
-        // setCount(prev => prev +1);
-        // setCount(prev => prev +1);
+        //--------------- overriding the value of count each time, bec we are using the same value of count each time
+            // setCount(count+1);
+            // setCount(count+1);
+            // setCount(count+1);
+
+        // --------------instead we can use the prev state value
+            // setCount(prev => prev +1);
+            // setCount(prev => prev +1);
+            // setCount(prev => prev +1);
+            // setCount(prev => prev +1);
+            // setCount(prev => prev +1);
+
+            setState(prev => {
+                return {
+                    count: prev.count +1
+                }
+            })
         
         
         
