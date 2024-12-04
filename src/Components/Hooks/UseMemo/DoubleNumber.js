53a8f0c5c65916e0a5794b13127fdca17039d7f3
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-04 11:47:28 +0530

    usememo and useref example doubleNumber

--- src/Components/Hooks/UseMemo/DoubleNumber.js
+++ src/Components/Hooks/UseMemo/DoubleNumber.js
@@ -1,18 +1,42 @@
-import { useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 
 export default function DoubleNumber(){
-    const [number, setNmuber] = useState();
-    const doubleNumber = () => {
+    const [number, setNmuber] = useState(0);
+    const [theme, setTheme] = useState(false);
+    const startTimeRef = useRef(0);
+
+    const doubleNumber = useMemo(() => {
+        startTimeRef.current = performance.now();
+        for(let i = 0; i < 1000;i++){}
+        const endtime = performance.now();
+        const dration = endtime - startTimeRef.current;
+        console.log(`time taken to change theme ${dration.toFixed(2)}ms`);
         return number * 2;
+    },[number]);
+
+    const handleChange = () => {
+        // startTimeRef.current = performance.now();
+        setTheme(!theme);
     }
-    const [theme, setTheme] = useState(false)
+
+    const themeStyle = {
+        backgroundColor: theme ? 'white': 'black',
+        color: theme ? 'black': 'white'
+    }
+
+    useEffect(()=>{
+        // const endtime = performance.now();
+        // const dration = endtime - startTimeRef.current;
+        console.log("theme changed");
+       
+        
+    },[theme]);
 
     return (
         <>
-            <input type = "text"  onChange={e => setNmuber(e.target.value)}/>
-            <button onClick={() => setTheme(!theme) }>Change Theme</button>
-            <div style={{backgroundColor: theme === true? 'white': 'black',
-                 color: theme === true? 'black': 'white'}}>{doubleNumber()}</div>
+            <input type = "text" value={number}  onChange={e => setNmuber(e.target.value)}/>
+            <button onClick={handleChange}>Change Theme</button>
+            <div style={themeStyle}>{doubleNumber}</div>
         </>
     )
 }
