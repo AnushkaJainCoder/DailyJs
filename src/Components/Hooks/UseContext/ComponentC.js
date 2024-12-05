a648eb03e75edabda5d522a3b20a0147c804ab4f
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-05 12:34:57 +0530

    created consumer component in component D

--- src/Components/Hooks/UseContext/ComponentC.js
+++ src/Components/Hooks/UseContext/ComponentC.js
@@ -2,15 +2,15 @@ import { useState } from "react"
 import ComponentD from "./ComponentD"
 // import '...//'
 
-export default function ComponentC(props){
+export default function ComponentC(){
     const [user, setUser] = useState('')
     return (
         <>
             <div style={{border: '2px solid ', padding: '25px'}}>
             <h1>Component 3</h1>
                 {/* <h1>Component 1</h1> */}
-                <h2>Hello {props.user}</h2>
-                <ComponentD user = {props.user}/>
+                <h2>Hello </h2>
+                <ComponentD />
             </div>
 
         </>
--- src/Components/Hooks/UseContext/ComponentD.js
+++ src/Components/Hooks/UseContext/ComponentD.js
@@ -1,14 +1,17 @@
-import { useState } from "react"
-// import '...//'
+import { useContext, useState } from "react"
+import { UserContext } from "./ComponentA"
 
 export default function ComponentD(props){
-    const [user, setUser] = useState('')
+
+    // const [user, setUser] = useState('');
+    const user = useContext(UserContext);
+
     return (
         <>
             <div style={{border: '2px solid ', padding: '25px'}}>
             {/* <h1>Component 2</h1> */}
                 <h1>Component 4</h1>
-                <h2>Hello {props.user}</h2>
+                <h2>Hello {user}</h2>
                 
             </div>
 
