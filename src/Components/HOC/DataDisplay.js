804e7c36980537f5ec867e77870e160f2be1ca4e
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-05 15:41:22 +0530

    add data to hoc

--- src/Components/HOC/DataDisplay.js
+++ src/Components/HOC/DataDisplay.js
@@ -3,7 +3,7 @@ export default function DataDisplay({data}){
     return (
         <>
             <h1>data: </h1>
-            <pre>{JSON.stringify(data,null,2)}</pre>
+            <div>{JSON.stringify(data,null,2)}</div>
         </>
     );
 };
